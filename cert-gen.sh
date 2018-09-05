be1af0c8707e4ff97078b9747ed43f8a88b4af56
iNviNho <vladino.vrab@gmail.com>
2018-09-05 21:27:44 +0000

    Updated nginx for certs path

--- cert-gen.sh
+++
@@ -1,1 +0,0 @@
-sudo certbot certonly --agree-tos --rsa-key-size 4096 -d hazarder.vladino.me -d www.hazarder.vladino.me --renew-by-default
--- rename-certs-for-ssl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mv /etc/letsencrypt/archive/hazarder.vladino.me/cert1.pem /etc/letsencrypt/archive/hazarder.vladino.me/hazarder.vladino.me.crt
-mv /etc/letsencrypt/archive/hazarder.vladino.me/privkey1.pem /etc/letsencrypt/archive/hazarder.vladino.me/hazarder.vladino.me.key
